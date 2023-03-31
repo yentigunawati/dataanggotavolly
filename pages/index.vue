773f04fce525e9aa7fc30890494d4de9f934326d
yentigunawati <122844871+yentigunawati@users.noreply.github.com>
2023-03-31 13:11:18 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1> dataanggotavolly</h1>
+    <Nuxt-link to="/tambah"> isi data </Nuxt-link>
+    <table border="1" width="50%">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>nama</th>
+          <th>kelas</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="visitor in datas" :key="visitor.id">
+        <td>{} visitor.id {}</td>
+        <td>{} visitor.nama {}</td>
+        <td>{} visitor.kelas {}</td>
+      </tr>
+     </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+const supabase=useSupabaseAuthClient()
+const datas= ref()
+
+async function getData() {
+  const { data, error } = await supabase
+  .from("dataanggotavolly")
+  .select()
+  datas.value = data
+}
+
+onMounted (() => {
+  getData()
+})
+</script>
