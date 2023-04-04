1f523cbaf38cf1c739a8d9dacdbb80a324d3aeb6
JouskaPram <pramudita5800@gmail.com>
2023-04-04 14:21:24 +0700

    ea

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,36 @@
 <template>
   <div>
-    <h1> dataanggotavolly</h1>
+    <h1>dataanggotavolly</h1>
     <Nuxt-link to="/tambah"> isi data </Nuxt-link>
     <table border="1" width="50%">
       <thead>
         <tr>
-          <th>#</th>
+          <th>no         </th>
           <th>nama</th>
           <th>kelas</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="visitor in datas" :key="visitor.id">
-        <td>{} visitor.id {}</td>
-        <td>{} visitor.nama {}</td>
-        <td>{} visitor.kelas {}</td>
-      </tr>
-     </tbody>
+        <tr v-for="(visitor, index) in datas" :key="visitor.id">
+          <td>{{ index + 1 }}</td>
+          <td>{{ visitor.nama }}</td>
+          <td>{{ visitor.kelas }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
 
 <script setup>
-const supabase=useSupabaseAuthClient()
-const datas= ref()
+const supabase = useSupabaseAuthClient();
+const datas = ref();
 
 async function getData() {
-  const { data, error } = await supabase
-  .from("dataanggotavolly")
-  .select()
-  datas.value = data
+  const { data, error } = await supabase.from("dataanggotavolly").select();
+  datas.value = data;
 }
 
-onMounted (() => {
-  getData()
-})
+onMounted(() => {
+  getData();
+});
 </script>
---
+++ pages/tambah.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>tambah data</h3>
+    <form @submit.prevent="addData">
+      <div>
+        <label for="">nama</label>
+        <input type="text" v-model="nama" />
+      </div>
+      <div>
+        <label for="">Kelas</label>
+        <input type="number" v-model="kelas" />
+      </div>
+      <button type="submit">tambah</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseAuthClient();
+const nama = ref();
+const kelas = ref();
+const addData = async () => {
+  const { data, error } = await supabase.from("dataanggotavolly").insert({
+    nama: nama.value,
+    kelas: kelas.value,
+  });
+  alert("berhasil tambah data");
+  navigateTo("/");
+  if (error) {
+    console.log(error);
+  }
+};
+</script>
