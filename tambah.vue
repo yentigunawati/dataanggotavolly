955e76a2091dcaa3c9779434320261873c7e3816
yentigunawati <122844871+yentigunawati@users.noreply.github.com>
2023-04-05 15:38:16 +0700

    Add files via upload

---
+++ tambah.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>isi data anggota</h1>
+    <form @submit.prevent="simpan">
+    <input v-model="id" > <br/>
+    <input v-model="nama" > <br/>
+    <textarea v-model="kelas" placeholder="kelas"></textarea> <br/>
+    <button type="submit">kirim</button>
+    <Nuxt-link to="/">kembali</Nuxt-link>
+    </form>
+   </div>
+</template>
+
+<script setup>
+const supabase= useSupabaseAuthClient()
+const id= ref()
+const nama= ref()
+const kelas= ref()
+async function simpan(){
+await supabase
+.from("dataangotavolly")
+.insert({
+  id: id.value,
+  nama: nama.value,
+  kelas: kelas.value,
+})
+const router= useRouter()
+router.push("/tambah") 
+}
+{
+
+}
+</script>
